1dd7738eae1f3005d7b4fb40c0bfed6e3a515a9a
sixiaoyou <yunshaliying@sina.com>
2017-05-11 17:32:39 +0800

    Add LeetCode561-Array Partition I

--- src/main/webapp/scripts/test.js
+++ src/main/webapp/scripts/test.js
@@ -1,3 +1,12 @@
-var dict=[]
-document.write(dict["a"]+1);
+function sortNumber(a,b){
+    return a-b;
+}
+a=[6214, -2290, 2833, -7908]
+a.sort(sortNumber)
+
+document.write("<br>");
+for(var i in a) {
+    document.write(a[i]);
+    document.write("<br>");
+}
 
